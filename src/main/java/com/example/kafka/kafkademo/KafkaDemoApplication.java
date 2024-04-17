16695735b018846740fbb32f5241ba155349b219
Arihant <arihant0996@gmail.com>
2024-04-17 15:05:20 -0300

    API endpoint to process kafka streams data

--- src/main/java/com/example/kafka/kafkademo/KafkaDemoApplication.java
+++ src/main/java/com/example/kafka/kafkademo/KafkaDemoApplication.java
@@ -10,12 +10,18 @@ import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.common.serialization.Serde;
 import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.KafkaStreams;
+import org.apache.kafka.streams.StoreQueryParameters;
 import org.apache.kafka.streams.StreamsBuilder;
 import org.apache.kafka.streams.kstream.Consumed;
 import org.apache.kafka.streams.kstream.Grouped;
 import org.apache.kafka.streams.kstream.KStream;
 import org.apache.kafka.streams.kstream.KTable;
+import org.apache.kafka.streams.kstream.Materialized;
 import org.apache.kafka.streams.kstream.Produced;
+import org.apache.kafka.streams.state.QueryableStoreType;
+import org.apache.kafka.streams.state.QueryableStoreTypes;
+import org.apache.kafka.streams.state.ReadOnlyKeyValueStore;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -24,12 +30,15 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.event.EventListener;
 import org.springframework.kafka.annotation.EnableKafkaStreams;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.config.StreamsBuilderFactoryBean;
 import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import lombok.RequiredArgsConstructor;
-import lombok.val;
 import net.datafaker.Faker;
 import reactor.core.publisher.Flux;
 
@@ -102,8 +111,23 @@ class Processor {
 		KTable<String, Long> wordsCount = data
 			.flatMapValues(value -> Arrays.asList(value.toLowerCase().split("\\W+")))
 			.groupBy((key, value) -> value, Grouped.with(stringSerde, stringSerde))
-			.count();
+			.count(Materialized.as("counts"));
 
 		wordsCount.toStream().to("streams-wordcount-output", Produced.with(stringSerde, longSerde));
 	}
+}
+
+@RestController
+@RequiredArgsConstructor
+class RestService {
+	private final StreamsBuilderFactoryBean factoryBean;
+
+	@GetMapping("/count/{word}")
+	public Long getCount(@PathVariable("word") String word) {
+		final KafkaStreams kafkaStreams = factoryBean.getKafkaStreams();
+
+		final ReadOnlyKeyValueStore<String, Long> counts = kafkaStreams.store(StoreQueryParameters.fromNameAndType("counts", QueryableStoreTypes.keyValueStore()));
+		return counts.get(word);
+	}
+
 }
