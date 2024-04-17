de22a56e1ab006e18d29b8738fef697f81be55aa
Arihant <arihant0996@gmail.com>
2024-04-17 14:38:44 -0300

    Process messages using kafka streams

--- src/main/java/com/example/kafka/kafkademo/KafkaDemoApplication.java
+++ src/main/java/com/example/kafka/kafkademo/KafkaDemoApplication.java
@@ -1,28 +1,40 @@
 package com.example.kafka.kafkademo;
 
 import java.time.Duration;
+import java.util.Arrays;
 import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.stream.Stream;
 
 import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.common.serialization.Serde;
+import org.apache.kafka.common.serialization.Serdes;
+import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.kstream.Consumed;
+import org.apache.kafka.streams.kstream.Grouped;
+import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.kstream.KTable;
+import org.apache.kafka.streams.kstream.Produced;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.event.EventListener;
+import org.springframework.kafka.annotation.EnableKafkaStreams;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
 import lombok.RequiredArgsConstructor;
+import lombok.val;
 import net.datafaker.Faker;
 import reactor.core.publisher.Flux;
-import reactor.util.function.Tuple2;
 
 @SpringBootApplication
+@EnableKafkaStreams
 public class KafkaDemoApplication {
 
 	public static void main(String[] args) {
@@ -33,6 +45,14 @@ public class KafkaDemoApplication {
 	NewTopic hobbit2() {
 		return TopicBuilder.name("hobbit2")
 			.partitions(12)
+			.replicas(3)
+			.build();
+	}
+
+	@Bean
+	NewTopic counts() {
+		return TopicBuilder.name("streams-wordcount-output")
+			.partitions(6)
 			.replicas(3)
 			.build();
 	}
@@ -64,5 +84,26 @@ class Consumer {
 	@KafkaListener(topics = {"hobbit"}, groupId = "spring-boot-kafka")
 	public void consume(ConsumerRecord<String, String> record) {
 		System.out.println("received quote = " + record.value() + " with key : " + record.key());
+	}
+}
+
+@Component
+class Processor {
+
+	@Autowired
+	public void process(StreamsBuilder builder) {
+
+		// Serializers/Deserializers (serde) 
+		final Serde<String> stringSerde = Serdes.String();
+		final Serde<Long> longSerde = Serdes.Long();
+
+		KStream<String, String> data = builder.stream("hobbit", Consumed.with(stringSerde, stringSerde));
+
+		KTable<String, Long> wordsCount = data
+			.flatMapValues(value -> Arrays.asList(value.toLowerCase().split("\\W+")))
+			.groupBy((key, value) -> value, Grouped.with(stringSerde, stringSerde))
+			.count();
+
+		wordsCount.toStream().to("streams-wordcount-output", Produced.with(stringSerde, longSerde));
 	}
 }
