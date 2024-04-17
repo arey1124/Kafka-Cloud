33255462743bf13532b16b3a5bd13fbff4e16cbd
Arihant <arihant0996@gmail.com>
2024-04-17 13:55:20 -0300

    Programatically create topics using topic builder

--- src/main/java/com/example/kafka/kafkademo/KafkaDemoApplication.java
+++ src/main/java/com/example/kafka/kafkademo/KafkaDemoApplication.java
@@ -5,12 +5,15 @@ import java.util.function.Function;
 import java.util.function.Supplier;
 import java.util.stream.Stream;
 
+import org.apache.kafka.clients.admin.NewTopic;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.event.ApplicationStartedEvent;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.event.EventListener;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.config.TopicBuilder;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Component;
 
@@ -24,6 +27,14 @@ public class KafkaDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(KafkaDemoApplication.class, args);
+	}
+
+	@Bean
+	NewTopic hobbit2() {
+		return TopicBuilder.name("hobbit2")
+			.partitions(12)
+			.replicas(3)
+			.build();
 	}
 
 }
